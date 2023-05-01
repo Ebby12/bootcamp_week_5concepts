7f16cbcb65347be647ee71fa64a44f6f9a80b9fc
Ebtsam <ebtsam.negash@gmail.com>
2023-05-01 11:45:41 -0400

    async and await added

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function main (){
+
+const path = require('path');
+const {readFile,writeFile} = require('fs').promises;
+
+ async function main (){
     let pokemon = {
     name:"Pica",
     type:"Electric"
@@ -7,6 +11,21 @@ function main (){
     let dbs = [];
 dbs.push(pokemon);
 console.log(dbs);
+console.log(__dirname);
+
+const buffer =  await readFile(path.join(__dirname,"pokemon.txt"));
+console.log(buffer);
+const db = JSON.parse(buffer)
+console.log(db);
+db.push(pokemon);
+
+console.log(db)
+
+const stringToSave = JSON.stringify(db);
+await writeFile(path.join(__dirname,"pokemon.txt"),stringToSave);
+console.log('Done');
+
+
 }
 
 
